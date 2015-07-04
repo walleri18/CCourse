7b56bd7ed7f197b1d519d427eb473ec9ce7cf4f0
walleri18 <walleri18@yandex.ru>
2015-07-04 21:19:10 +0400

    5

--- Practika1GL7.cpp
+++ Practika1GL7.cpp
@@ -7,12 +7,22 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
-void spisok();
-void list(int n);
-void clear();
+//Рабочие методы
+void spisok(); //Вывод списка
+void list(int n); //Выбор пользователя
+void clear();// метод для очистки
+//Творческие методы
+void song(); //метод вызова песни 99 бутылок пива на столе
+void calculate();//метод простого калькулятора
+//Прототипы калькулятора
+double summa(double args1, double args2);
+double delition(double args1, double args2);
+double mult(double args1, double args2);
+double raznost(double args1, double args2);
 
 int main() 
 {
@@ -26,7 +36,7 @@ int main()
     cin >> menu;
     cout << endl;
     
-    if (menu > 5 || menu < 1) spisok();
+    if (menu > 2 || menu < 1) spisok();
     else list(menu);
     
     cout << endl << "Хотите выйти? (да - 1, нет - любая): ";
@@ -46,14 +56,76 @@ void spisok()
 
 void list(int n)
 {
-    if (n == 1) cout << "Вы выбрали Саров";
-    else if (n == 2) cout << "Вы выбрали Москву";
-    else if (n == 3) cout << "Вы выбрали Нижний Новгород";
-    else if (n == 4) cout << "Вы выбрали Санкт-Петербург";
-    else if (n == 5) cout << "Вы выбрали университет ННГУ им. Лобачевского";
+    if (n == 1) calculate();
+    else if (n == 2) song();
 }
 
 void clear()
 {
     for (int i = 0; i < 1000; i++) cout << endl;
+}
+
+void song()
+{
+    cout << endl << "Песня 99 бутылок пива на столе:"<< endl;
+    
+    for (int i = 99; i >= 2; i--)
+    {
+        cout << i << " bottles of beer on the wall" << endl;
+        cout << i << " bottles of beer!" << endl;
+        cout << "Take one down, pass it around" << endl;
+        cout << (i - 1) << " bottles of beer on the wall!" << endl;
+    }
+}
+
+double summa(double args1, double args2)
+{
+    double summa = (args1 + args2);
+    return summa;
+}
+
+double raznost(double args1, double args2)
+{
+    double raznost = (args1 - args2);
+    return raznost;
+}
+
+double mult(double args1, double args2)
+{
+    double mult = (args1 * args2);
+    return mult;
+}
+
+double delition(double args1, double args2)
+{
+    while (args2 == 0) 
+    {
+        cout << "ОШИБКА!!! (ДЕЛЕНИЕ НА НОЛЬ!!!)" << endl << "Введите второй аргумент заново: ";
+        cin >> args2;
+    }
+    double delition = (args1 / args2);
+    return delition;
+}
+
+void calculate()
+{
+    char operation;
+    double args1, args2, result;
+    
+    cout << "Добро пожаловать в калькулятор!!!" << endl;
+    cout << "Введите арифметическую операцию: ";
+    cin >> operation;
+    
+    cout << endl << "Введите первый аргумент: ";
+    cin >> args1;
+    
+    cout << endl << "Введите второй аргумент: ";
+    cin >> args2;
+    
+    if (operation == '+') result = summa(args1, args2);
+    else if (operation == '-') result = raznost(args1, args2);
+    else if (operation == '*') result = mult(args1, args2);
+    else if (operation == '/') result = delition(args1, args2);
+    
+    cout << endl << "Результат выполнении операции: " << args1 << " " << operation << " " << args2 << " = " << result << endl;
 }
