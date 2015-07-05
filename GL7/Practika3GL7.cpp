017635cbd7783f49c471f93192f56190471bf4a3
walleri18 <walleri18@yandex.ru>
2015-07-05 16:39:50 +0400

    19

--- GL7/Practika3GL7.cpp
+++ GL7/Practika3GL7.cpp
@@ -17,15 +17,43 @@ enum Board
     krestik = 2 //переменная для обозначения присутствия крестика в клетке
 };
 
+enum Doska
+{
+    kletka1 = Board.pusto,
+    kletka2 = Board.pusto,
+    kletka3 = Board.pusto,
+    kletka4 = Board.pusto,
+    kletka5 = Board.pusto,
+    kletka6 = Board.pusto,
+    kletka7 = Board.pusto,
+    kletka8 = Board.pusto,
+    kletka9 = Board.pusto
+};
 
+enum Player
+{
+    fisrt = 0,
+    second = 0
+};
+
+void BoardList();
 
 int main()
 {
+    cout << "Добро пожаловать в игру крестики нолики для двух игроков!!!" << endl;
     
-    
+    BoardList();
     
     
     
     
     return 0;
+}
+
+void BoardList()
+{
+    cout << endl << "Вид доски" << endl;
+    cout << Doska.kletka1 << " " << Doska.kletka2 << " " << Doska.kletka3 << endl;
+    cout << Doska.kletka4 << " " << Doska.kletka5 << " " << Doska.kletka6 << endl;
+    cout << Doska.kletka7 << " " << Doska.kletka8 << " " << Doska.kletka9 << endl;
 }
--- build/Debug/Cygwin_4.x-Windows/GL7/Practika3GL7.o.d
+++
@@ -1,1 +0,0 @@
-build/Debug/Cygwin_4.x-Windows/GL7/Practika3GL7.o: GL7/Practika3GL7.cpp
