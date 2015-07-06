91026b29a954cd61e1fb35d47749295a7c9e2e29
walleri18 <walleri18@yandex.ru>
2015-07-06 12:48:15 +0400

    28

--- GL8/Practika1GL8.cpp
+++ GL8/Practika1GL8.cpp
@@ -1,36 +1,36 @@
-/* 
- * File:   Practika1GL8.cpp
- * Author: Туров Виталий
- *
- * Created on 6 Июль 2015 г., 11:20
- */
-
-#include <cstdlib>
-#include <iostream>
-#include <ctime>
-#include <cstdlib>
-#include <string>
-
-using namespace std;
-
-int main() 
-{
-    cout << "Добро пожаловать в программу имитации подбрасывания монеты!" << endl;
-    int moneta, vibor;
-    string monetaS;
-    
-    do
-    {
-    srand(time(NULL));
-    moneta = rand() % 2;
-    
-    if (moneta == 1) monetaS = "Орёл";
-    else monetaS = "Решка";
-    
-    cout << endl << monetaS << endl;
-    cout << "Для заверения введите 1, для продолжения любую клавишу: ";
-    cin >> vibor;
-    }while(vibor != 1);
-        
-    return 0;
-}
+///* 
+// * File:   Practika1GL8.cpp
+// * Author: Туров Виталий
+// *
+// * Created on 6 Июль 2015 г., 11:20
+// */
+//
+//#include <cstdlib>
+//#include <iostream>
+//#include <ctime>
+//#include <cstdlib>
+//#include <string>
+//
+//using namespace std;
+//
+//int main() 
+//{
+//    cout << "Добро пожаловать в программу имитации подбрасывания монеты!" << endl;
+//    int moneta, vibor;
+//    string monetaS;
+//    
+//    do
+//    {
+//    srand(time(NULL));
+//    moneta = rand() % 2;
+//    
+//    if (moneta == 1) monetaS = "Орёл";
+//    else monetaS = "Решка";
+//    
+//    cout << endl << monetaS << endl;
+//    cout << "Для заверения введите 1, для продолжения любую клавишу: ";
+//    cin >> vibor;
+//    }while(vibor != 1);
+//        
+//    return 0;
+//}
---
+++ GL8/Practika2GL8.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   Practika2GL8.cpp
+ * Author: Туров Виталий
+ *
+ * Created on 6 Июль 2015 г., 12:47
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
