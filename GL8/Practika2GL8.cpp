509770464826122a1065b997db798e935a1f39a5
walleri18 <walleri18@yandex.ru>
2015-07-06 13:02:59 +0400

    30

--- GL8/Practika2GL8.cpp
+++ GL8/Practika2GL8.cpp
@@ -14,12 +14,28 @@ using namespace std;
 
 int main()
 {
-    
+    int vibor;
+    do
+    {
     srand(time(NULL));
     
     int chisloKomp = rand() % 101;
-    cout << chisloKomp << endl;
-    
+    int chelovek, popitka = 1;
+    cout << "Добро пожаловать в игру угадай число! (от 1 до 100)" << endl;
+    cout << "Введите задуманное число: ";
+    cin >> chelovek;
+    while (chisloKomp != chelovek)
+    {
+        if (chisloKomp > chelovek) cout << endl << "Побольше" << endl;
+        else cout << endl << "Поменьше";
+        cout << endl << "Неправильно. Введите заново: ";
+        cin >> chelovek;
+        popitka++;
+    }
+    cout << "Ура вы угадачи число: " << chisloKomp << " с попытки №" << popitka << endl;
+    cout << "Хотите продолжить? ( да - 1, нет - 0 ): ";
+    cin >> vibor;
+    }while(vibor == 1);
     return 0;
 }
 
